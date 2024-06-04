67f4b3a4ff090bdc04d0a7610ac84d284ca295a5
sebaspa0107 <sebaspa0107@gmail.com>
2024-06-03 21:43:31 -0500

    🎉 Plantilla inicial

--- themes/acomedios/footer.php
+++ themes/acomedios/footer.php
@@ -1,34 +1,37 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package acomedios
- */
-
-?>
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'acomedios' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'acomedios' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'acomedios' ), 'acomedios', '<a href="https://github.com/sebaspa">Sebaspa</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+<footer class="footerPartners">
+  <div class="container footerPartners__container">
+    <p class="footerPartners__text">Partners oficiales</p>
+    <div class="footerPartners__imgs">
+      <img src="https://dummyimage.com/230x80/fff/000" alt="image" />
+      <img src="https://dummyimage.com/150x80/fff/000" alt="image" />
+      <img src="https://dummyimage.com/150x80/fff/000" alt="image" />
+    </div>
+  </div>
+</footer>
+<footer class="footerContact">
+  <div class="container footerContact__container">
+    <img src="https://dummyimage.com/180x80/fff/000" alt="image" />
+    <p class="footerContact__text">Política de privacidad Derechos Reservados Acomedios &copy;2024</p>
+    <p class="footerContact__address">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+        <path fill="currentColor"
+          d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+      </svg>
+      Cl. 99 #7A-77, Bogotá
+    </p>
+    <p class="footerContact__phone">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path fill="currentColor"
+          d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+      </svg>
+      (601) 617 0956
+    </p>
+    <div class="footerContact__email">
+      <p>Tu dirección de correo</p>
+    </div>
+  </div>
+</footer>
 <?php wp_footer(); ?>
-
 </body>
+
 </html>
