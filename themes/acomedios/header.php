938ddfa4134512ba76d210b218a57a7192632c33
Julian Valencia <julianvf@gmail.com>
2024-06-06 21:41:59 -0500

    se carga un menu y varias imagenes, aun estoy en pruebas

--- themes/acomedios/header.php
+++ themes/acomedios/header.php
@@ -12,3 +12,17 @@
 
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
+  <nav>
+    <ul class="menu">
+      <li class="logo"><a href="#">Creative Mind Agency</a></li>
+      <li class="item"><a href="#">Home</a></li>
+      <li class="item"><a href="#">About</a></li>
+      <li class="item has-submenu"><a tabindex="0">Services</a></li>
+      <li class="item has-submenu"><a tabindex="0">Plans</a></li>
+      <li class="item"><a href="#">Blog</a></li>
+      <li class="item"><a href="#">Contact</a></li>
+      <li class="item button"><a href="#">Log In</a></li>
+      <li class="item button secondary"><a href="#">Sign Up</a></li>
+      <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
+    </ul>
+</nav>
