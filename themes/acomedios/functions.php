ee0da7f65d55206d480bb817b9760043e782fcc8
Julian Valencia <julianvf@gmail.com>
2024-06-07 21:28:47 -0500

    se ajusta el menú en escritorio, se le da color, hover y se adicionan los componentes

--- themes/acomedios/functions.php
+++ themes/acomedios/functions.php
@@ -9,18 +9,7 @@ function acomedios_setup()
   load_theme_textdomain('acomedios', get_template_directory() . '/languages');
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
-  add_theme_support(
-    'html5',
-    array(
-      //'search-form',
-      //'comment-form',
-      //'comment-list',
-      //'gallery',
-      //'caption',
-      //'style',
-      //'script',
-    )
-  );
+  add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script' ) );
 
   register_nav_menus(
     array(
--- themes/acomedios/index.php
+++ themes/acomedios/index.php
@@ -1,13 +1,22 @@
-<?php get_header(); ?>
-<?php
+<?php 
+
+get_header();
+
+/*
 if (have_posts()):
   while (have_posts()):
     the_post();
     the_content();
   endwhile;
 endif;
+*/
 ?>
+
+<?php get_template_part('template-parts/content', 'menu-header' ); ?>
+
 <div class="bgPhone">
   <div class="bgPhone__img"></div>
 </div>
-<?php get_footer(); ?>
+<?php
+  get_footer();
+?>
